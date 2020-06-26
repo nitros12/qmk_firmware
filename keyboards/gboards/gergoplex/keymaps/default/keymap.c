507c277903c8bbaece3349953c515a326d4af196
Ben Simms <ben@bensimms.moe>
2020-06-26 04:53:36 +0100

    Push changes

--- keyboards/gboards/gergoplex/keymaps/default/keymap.c
+++ keyboards/gboards/gergoplex/keymaps/default/keymap.c
@@ -52,9 +52,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Keymap 1: Symbols layer
      * +------------------------------+      +--------------------------------+
-     * |  !   |  @  |  {  |  }  |  |  |      |  `  |  ~  | ___ | ___ |    \   |
+     * |  !   |  @  |  {  |  }  |  |  |      |  `  |  ~  |  \  |     |   "    |
      * |-----+-----+-----+-----+------|      |--------------------------------|
-     * |  #   |  $  |  (  |  )  | LMB |      |  +  |  -  |  /  |  *  |    '   |
+     * |  #   |  $  |  (  |  )  | LMB |      |  +  |  -  |  /  |  *  |   '    |
      * |-----+-----+-----+-----+------+		   |--------------------------------|
      * |  %   |  ^  |  [  |  ]  | RMB |      |  &  |  =  |  ,  |  .  |   -    |
      * `------+-----+-----+------+----+		   +--------------------------------+
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *  				|MMB |  ;  	|	 = 	|						|  =  |  ;  |  DEL |
      *  				'-----------------'           '------------------'
      */
-    [SYMB] = LAYOUT_gergoplex(KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,    KC_GRV,  KC_TILD, KC_TRNS, KC_TRNS, KC_BSLS,
+    [SYMB] = LAYOUT_gergoplex(KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,    KC_GRV,  KC_TILD, KC_BSLS, KC_TRNS, KC_DQT,
                               KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_BTN2,    KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_QUOT, 
                               KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_BTN1,    KC_AMPR, KC_EQL,  KC_COMM, KC_DOT,  KC_MINS, 
 

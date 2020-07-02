6f66d3cb5afaafef5305cb83025c92acf301e84c
Ben Simms <ben@bensimms.moe>
2020-07-02 05:34:27 +0100

    Push changes

--- keyboards/gboards/gergoplex/keymaps/default/keymap.c
+++ keyboards/gboards/gergoplex/keymaps/default/keymap.c
@@ -73,14 +73,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----+-----+-----+-----+------|      |--------------------------------|
      * |  F1  | F2  | F3  | F4  |  F5 |      | LFT | DWN  | UP  | RGT | VOLUP |
      * |-----+-----+-----+-----+------+		   |--------------------------------|
-     * |  F6  | F7  | F8  | F9  | F10 |      | PGDN|C(DWN)|C(UP)| PGUP| VOLDN |
+     * |CTL/F6| F7  | F8  | F9  | F10 |      | PGDN|C(DWN)|C(UP)| PGUP| VOLDN |
      * `------+-----+-----+------+----'		   `--------------------------------'
      *  				.-----------------.           .-----------------.
-     *  				| F11 | F12|	  	|						|     | ALT |     |
+     *  				| F11 | F12|	  	|						|     | ALT | END |
      *  				'-----------------'           '-----------------'
      */
     [NUMB] = LAYOUT_gergoplex(KC_1,  KC_2,  KC_3,   KC_4,   KC_5,     KC_6,    KC_7,       KC_8,     KC_9,    KC_0,
                               KC_F1, KC_F2, KC_F3,  KC_F4,  KC_F5,    KC_LEFT, KC_DOWN,    KC_UP,    KC_RGHT, KC_VOLU,
-                              KC_F6, KC_F7, KC_F8,  KC_F9,  KC_F10,   KC_PGDN, C(KC_DOWN), C(KC_UP), KC_PGUP, KC_VOLD, 
+                              MT(MOD_LCTL, KC_F6), KC_F7, KC_F8,  KC_F9,  KC_F10,   KC_PGDN, C(KC_DOWN), C(KC_UP), KC_PGUP, KC_VOLD, 
 
-                                            KC_F11, KC_F12, KC_TRNS,  KC_TRNS, KC_LALT,    KC_TRNS)};
+                                            KC_F11, KC_F12, KC_TRNS,  KC_TRNS, KC_LALT,    KC_END)};
